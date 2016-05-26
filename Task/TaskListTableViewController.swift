9940ce63da5f8c1f8cde81ada76b428d4bff8bb1
kpfister <kpfister@me.com>
2016-05-26 15:45:07 -0600

    List View Editing
    completed the List View Editing section

---
+++ Task/TaskListTableViewController.swift
@@ -0,0 +1,113 @@
+//
+//  TaskListTableViewController.swift
+//  Task
+//
+//  Created by Karl Pfister on 5/26/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class TaskListTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        guard let sections = TaskController.sharedInstance.fectchedResultsController.sections
+            else {
+                return 0
+        }
+        return sections.count
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let sections = TaskController.sharedInstance.fectchedResultsController.sections
+            else {
+                return 0
+        }
+        return sections[section].numberOfObjects
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("toTaskDetail", forIndexPath: indexPath)
+        
+        guard let task = TaskController.sharedInstance.fectchedResultsController.objectAtIndexPath(indexPath) as? Task
+            else {
+                return UITableViewCell()
+        }
+        //        cell.textLabel?.text = task.title
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        guard let sections = TaskController.sharedInstance.fectchedResultsController.sections,
+            index = Int(sections[section].name) else {
+                return nil
+        }
+        if index == 0 {
+            return "Incomplete"
+        } else {
+            return "Complete"
+        }
+    }
+    
+    
+     // Override to support editing the table view.
+     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+     if editingStyle == .Delete {
+     // Delete the row from the data source
+     tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+     } else if editingStyle == .Insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+    
+    
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+     
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "toDetailView" {
+            guard let indexPath = tableView.indexPathForSelectedRow,
+            task = TaskController.sharedInstance.fectchedResultsController.objectAtIndexPath(indexPath) as? Task
+                else {
+                    return
+            }
+            let destinationVC = segue.destinationViewController as? TaskDetailTableViewController
+            destinationVC?.task = task
+        }
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
